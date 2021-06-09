ee5101e6978b5a0f3982b55404ec6b9a1aa269c4
vitorbrack <vitorodmiranda@gmail.com>
2021-06-09 09:43:08 -0300

    hotel

--- js/hotel.js
+++ js/hotel.js
@@ -1,10 +1,19 @@
 var hotel = {
-    nome : 'hakuna matata',
+    nome : ' Hotel Hakuna Matata',
     quartos :40,
     reservados : 25,
     chacarDisponinibilidade: function(){
    return this.quartos - this.reservados;
  }};
+
+var nomes = document.getElementById('nomes')
+nomes.textContent = hotel.nome;
+
+var room = document.getElementById('room')
+room.textContent = hotel.quartos;
+
+var reserv = document.getElementById('reserv')
+reserv.textContent = hotel.reservados;
 
 
 
