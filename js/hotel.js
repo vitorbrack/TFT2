4392fba9516003eb177d0d571ab3b88e31ea165a
vitorbrack <vitorodmiranda@gmail.com>
2021-06-10 11:50:47 -0300

    atualização do hotel

--- js/hotel.js
+++ js/hotel.js
@@ -25,6 +25,26 @@ function verificaVagas(){
   }
   
 }
+var countDownDate = new Date("Jun 10, 2021 11:50:00").getTime();
+var x = setInterval(function() {
+
+  var now = new Date().getTime();
+
+  var distance = countDownDate - now;
+
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  document.getElementById("data").innerHTML = days + "d " + hours + "h "
+  + minutes + "m " + seconds + "s ";
+
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("data").innerHTML = "Acabou a promoção!";
+  }
+}, 1000);
  
 
 
